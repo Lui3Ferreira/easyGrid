e32d406781a04c79fb2a07c8d47788774fc3c0de
DESKTOP-QSNTDL7\luigi <luigii_duarte@hotmail.com>
2022-09-23 00:33:22 +0100

    refactoring

--- Assets/UnityDemo/GridTest.cs
+++ Assets/UnityDemo/GridTest.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class GridTest : MonoBehaviour
 {
     private IGenerator gridGenerator;
+    //Reference of your empty game object
     public GameObject GameContainer;
+    //List of game objects that you want to display in your scene
     public List<GameObject> MyGameItems = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        //Create a grid object and add an epty object as parameter
+        //Create a grid object and add an empty object as a parameter
         gridGenerator = new GridGenerator(GameContainer);
 
         //Setup grid parameters by adding the array index position in the JSON FILE
         gridGenerator.SetUpJsonGrid(0);
 
-        //Add your list items to new grid!
+        //Add your list items to the new grid!
         gridGenerator.AddItemsToGrid(MyGameItems);
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # easyGrid
-A tool that allows easy creation and configuration of grids in a 3D space. The software was developed in Unity 2020.2.3f1 (64-bit).
+"easyGrid" is a tool that allows the easy creation and configuration of grids in a 3D space. The user can create various grid configurations to be later used in different game-like contexts. The software was developed in Unity 2020.2.3f1 (64-bit).
 
 If you are creating more extensive grids, but there are not enough game objects to visualize the results, you need to do the following:
 1-Open the GridGenerator.unity file.
