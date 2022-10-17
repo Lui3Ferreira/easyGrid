a0a8e64125a03c80a077ee615d747df174ad33b7
Lui3Ferreira <luis_ferreira@AirLuigi.local>
2022-10-17 15:36:55 +0100

    update namespace in doc. and added pdf documentation

--- Assets/NeuroRehabLab/easyGrid/UnityDemo/GridTest.cs
+++ Assets/NeuroRehabLab/easyGrid/UnityDemo/GridTest.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 using GenerateGrids;
 using GeneratorInterface;
 
-public class GridTest : MonoBehaviour
+namespace TestScript
 {
-    private IGenerator gridGenerator;
-    //Reference of your empty game object
-    public GameObject GameContainer;
-    //List of game objects that you want to display in your scene
-    public List<GameObject> MyGameItems = new List<GameObject>();
-
-    // Start is called before the first frame update
-    void Start()
+    public class GridTest : MonoBehaviour
     {
+        private IGenerator gridGenerator;
+        //Reference of your empty game object
+        [SerializeField]
+        private GameObject _gameContainer;
+        //List of game objects that you want to display in your scene
+        [SerializeField]
+        private List<GameObject> _myGameItems = new List<GameObject>();
+
+        // Start is called before the first frame update
+        public void Start()
+        {
         //Create a grid object and add an empty object as a parameter
-        gridGenerator = new GridGenerator(GameContainer);
+            gridGenerator = new GridGenerator(_gameContainer);
 
-        //Setup grid parameters by adding the array index position in the JSON FILE
-        gridGenerator.SetUpJsonGrid(0);
+            //Setup grid parameters by adding the array index position in the JSON FILE
+            gridGenerator.SetUpJsonGrid(0);
 
-        //Add your list items to the new grid!
-        gridGenerator.AddItemsToGrid(MyGameItems);
+            //Add your list items to the new grid!
+            gridGenerator.AddItemsToGrid(_myGameItems);
+        }
     }
 }
+
+
